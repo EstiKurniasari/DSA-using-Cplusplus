dd3eed9376d5bc5d3416727af6eee713430c3ae5
estikurniasari <estikurnia11@gmail.com>
2019-12-29 00:26:10 +0700

    - selection sort (ascending and descending)

--- 1-Array/SelectionSort/SelectionOfArray.cpp
+++ 1-Array/SelectionSort/SelectionOfArray.cpp
@@ -3,7 +3,7 @@ using namespace std;
 
 int main()
 {
-    int array[100], n, position, value;
+    int array[100], n, position, value, j;
 
     printf("Enter number of array elements\n");
     scanf("%d", &n);
@@ -20,15 +20,53 @@ int main()
     {
         printf("%d ", array[i]);
     }
+/*
+    // sorting ascending
+    for (int i = 0; i < n-1; i++)
+    {
+        position = i;
+        for (j = i+1; j < n; j++)
+        {
+            if (array[position] < array[j])
+            {
+                position = j;
+            }
+        }
+        if (position != i)
+        {
+            int temp;
+            temp = array[i];
+            array[i] = array[position];
+            array[position] = temp;
+        }
+    }
+*/
+        // sorting descending
+    for (int i = 0; i < n-1; i++)
+    {
+        position = i;
+        for (j = i+1; j < n; j++)
+        {
+            printf("\ni - pos : %d - %d\n", i, position);
 
-    // sorting
-    
+            if (array[position] > array[j])  
+            {
+                position = j;
+            }
+        }
+        if (position != i)
+        {
+            int temp;
+            temp = array[i];
+            array[i] = array[position];
+            array[position] = temp;
+        }
+    }
 
     printf("\nYour sorted array:\n");
 
     for (int i = 0; i < n; i ++)
     {
         printf("%d ", array[i]);
-        
     }
 }
